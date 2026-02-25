47b521f87fecc86519c8ab07b6ce294f7f43bca7
Adam Rizq Rozman <aroz0008@student.monash.edu>
2026-02-25 17:51:49 +0800

    fix: Remove capture attribute from file input in PosterUploader component

--- src/components/PosterUploader.tsx
+++ src/components/PosterUploader.tsx
@@ -105,7 +105,6 @@ export default function PosterUploader({ onExtracted }: PosterUploaderProps) {
             id="poster-input"
             type="file"
             accept="image/*"
-            capture="environment"
             className="hidden"
             onChange={(e) => e.target.files?.[0] && handleFile(e.target.files[0])}
           />
